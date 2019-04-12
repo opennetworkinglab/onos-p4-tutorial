22f55b5b39f6f29268c0e2ccd7d21e336b9df410
Carmelo Cascone <carmelo@ccascone.net>
2019-04-11 17:56:48 -0700

    First stap at ONOS app and pipeconf

---
+++ app/src/main/java/org/p4/p4d2/tutorial/P4InfoConstants.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2019-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.p4.p4d2.tutorial;
+
+import org.onosproject.net.pi.model.PiActionId;
+import org.onosproject.net.pi.model.PiActionParamId;
+import org.onosproject.net.pi.model.PiActionProfileId;
+import org.onosproject.net.pi.model.PiPacketMetadataId;
+import org.onosproject.net.pi.model.PiCounterId;
+import org.onosproject.net.pi.model.PiMatchFieldId;
+import org.onosproject.net.pi.model.PiTableId;
+/**
+ * P4Info constants.
+ */
+public final class P4InfoConstants {
+
+    // hide default constructor
+    private P4InfoConstants() {
+    }
+
+    // Header field IDs
+    public static final PiMatchFieldId HDR_ETHERNET_ETHER_TYPE =
+            PiMatchFieldId.of("hdr.ethernet.ether_type");
+    public static final PiMatchFieldId HDR_FABRIC_METADATA_L4_DST_PORT =
+            PiMatchFieldId.of("fabric_metadata.l4_dst_port");
+    public static final PiMatchFieldId HDR_ETHERNET_SRC_ADDR =
+            PiMatchFieldId.of("hdr.ethernet.src_addr");
+    public static final PiMatchFieldId HDR_ICMP_ICMP_TYPE =
+            PiMatchFieldId.of("hdr.icmp.icmp_type");
+    public static final PiMatchFieldId HDR_STANDARD_METADATA_INGRESS_PORT =
+            PiMatchFieldId.of("standard_metadata.ingress_port");
+    public static final PiMatchFieldId HDR_IPV6_DST_ADDR =
+            PiMatchFieldId.of("hdr.ipv6.dst_addr");
+    public static final PiMatchFieldId HDR_FABRIC_METADATA_IP_PROTO =
+            PiMatchFieldId.of("fabric_metadata.ip_proto");
+    public static final PiMatchFieldId HDR_FABRIC_METADATA_L4_SRC_PORT =
+            PiMatchFieldId.of("fabric_metadata.l4_src_port");
+    public static final PiMatchFieldId HDR_ETHERNET_DST_ADDR =
+            PiMatchFieldId.of("hdr.ethernet.dst_addr");
+    public static final PiMatchFieldId HDR_ICMP_ICMP_CODE =
+            PiMatchFieldId.of("hdr.icmp.icmp_code");
+    // Table IDs
+    public static final PiTableId FABRIC_INGRESS_L3_TABLE =
+            PiTableId.of("FabricIngress.l3_table");
+    public static final PiTableId FABRIC_INGRESS_SRV6_TRANSIT =
+            PiTableId.of("FabricIngress.srv6_transit");
+    public static final PiTableId FABRIC_INGRESS_L2_TABLE =
+            PiTableId.of("FabricIngress.l2_table");
+    public static final PiTableId FABRIC_INGRESS_L2_MY_STATION =
+            PiTableId.of("FabricIngress.l2_my_station");
+    public static final PiTableId FABRIC_INGRESS_SRV6_MY_SID =
+            PiTableId.of("FabricIngress.srv6_my_sid");
+    public static final PiTableId FABRIC_INGRESS_ACL =
+            PiTableId.of("FabricIngress.acl");
+    // Action IDs
+    public static final PiActionId FABRIC_INGRESS_SRV6_END =
+            PiActionId.of("FabricIngress.srv6_end");
+    public static final PiActionId FABRIC_INGRESS_SRV6_POP =
+            PiActionId.of("FabricIngress.srv6_pop");
+    public static final PiActionId FABRIC_INGRESS_SET_L2_NEXT_HOP =
+            PiActionId.of("FabricIngress.set_l2_next_hop");
+    public static final PiActionId FABRIC_INGRESS_PUNT_TO_CPU =
+            PiActionId.of("FabricIngress.punt_to_cpu");
+    public static final PiActionId FABRIC_INGRESS_L2_UNICAST_FWD =
+            PiActionId.of("FabricIngress.l2_unicast_fwd");
+    public static final PiActionId FABRIC_INGRESS_SRV6_T_INSERT_3 =
+            PiActionId.of("FabricIngress.srv6_t_insert_3");
+    public static final PiActionId NO_ACTION = PiActionId.of("NoAction");
+    public static final PiActionId FABRIC_INGRESS_CLONE_TO_CPU =
+            PiActionId.of("FabricIngress.clone_to_cpu");
+    public static final PiActionId FABRIC_INGRESS_DROP =
+            PiActionId.of("FabricIngress.drop");
+    public static final PiActionId FABRIC_INGRESS_SRV6_T_INSERT_2 =
+            PiActionId.of("FabricIngress.srv6_t_insert_2");
+    public static final PiActionId FABRIC_INGRESS_L2_MULTICAST_FWD =
+            PiActionId.of("FabricIngress.l2_multicast_fwd");
+    // Action Param IDs
+    public static final PiActionParamId DMAC = PiActionParamId.of("dmac");
+    public static final PiActionParamId S3 = PiActionParamId.of("s3");
+    public static final PiActionParamId S2 = PiActionParamId.of("s2");
+    public static final PiActionParamId S1 = PiActionParamId.of("s1");
+    public static final PiActionParamId PORT_NUM =
+            PiActionParamId.of("port_num");
+    public static final PiActionParamId GID = PiActionParamId.of("gid");
+    // Action Profile IDs
+    public static final PiActionProfileId FABRIC_INGRESS_ECMP_SELECTOR =
+            PiActionProfileId.of("FabricIngress.ecmp_selector");
+    // Packet Metadata IDs
+    public static final PiPacketMetadataId INGRESS_PORT =
+            PiPacketMetadataId.of("ingress_port");
+    public static final PiPacketMetadataId EGRESS_PORT =
+            PiPacketMetadataId.of("egress_port");
+}
---
+++ app/src/main/java/org/p4/p4d2/tutorial/pipeconf/InterpreterImpl.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright 2019-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.p4.p4d2.tutorial.pipeconf;
+
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableList;
+import org.onlab.packet.DeserializationException;
+import org.onlab.packet.Ethernet;
+import org.onlab.util.ImmutableByteSequence;
+import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.Port;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.device.DeviceService;
+import org.onosproject.net.driver.AbstractHandlerBehaviour;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.flow.criteria.Criterion;
+import org.onosproject.net.flow.instructions.Instruction;
+import org.onosproject.net.packet.DefaultInboundPacket;
+import org.onosproject.net.packet.InboundPacket;
+import org.onosproject.net.packet.OutboundPacket;
+import org.onosproject.net.pi.model.PiActionId;
+import org.onosproject.net.pi.model.PiMatchFieldId;
+import org.onosproject.net.pi.model.PiPipelineInterpreter;
+import org.onosproject.net.pi.model.PiTableId;
+import org.onosproject.net.pi.runtime.PiAction;
+import org.onosproject.net.pi.runtime.PiPacketMetadata;
+import org.onosproject.net.pi.runtime.PiPacketOperation;
+
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
+import static org.onlab.util.ImmutableByteSequence.copyFrom;
+import static org.onosproject.net.PortNumber.CONTROLLER;
+import static org.onosproject.net.PortNumber.FLOOD;
+import static org.onosproject.net.flow.instructions.Instruction.Type.OUTPUT;
+import static org.onosproject.net.flow.instructions.Instructions.OutputInstruction;
+import static org.onosproject.net.pi.model.PiPacketOperationType.PACKET_OUT;
+import static org.p4.p4d2.tutorial.P4InfoConstants.EGRESS_PORT;
+import static org.p4.p4d2.tutorial.P4InfoConstants.FABRIC_INGRESS_ACL;
+import static org.p4.p4d2.tutorial.P4InfoConstants.FABRIC_INGRESS_CLONE_TO_CPU;
+import static org.p4.p4d2.tutorial.P4InfoConstants.FABRIC_INGRESS_DROP;
+import static org.p4.p4d2.tutorial.P4InfoConstants.FABRIC_INGRESS_PUNT_TO_CPU;
+import static org.p4.p4d2.tutorial.P4InfoConstants.HDR_ETHERNET_DST_ADDR;
+import static org.p4.p4d2.tutorial.P4InfoConstants.HDR_ETHERNET_ETHER_TYPE;
+import static org.p4.p4d2.tutorial.P4InfoConstants.HDR_ETHERNET_SRC_ADDR;
+import static org.p4.p4d2.tutorial.P4InfoConstants.HDR_IPV6_DST_ADDR;
+import static org.p4.p4d2.tutorial.P4InfoConstants.HDR_STANDARD_METADATA_INGRESS_PORT;
+import static org.p4.p4d2.tutorial.P4InfoConstants.INGRESS_PORT;
+import static org.p4.p4d2.tutorial.P4InfoConstants.NO_ACTION;
+
+
+/**
+ * Interpreter implementation.
+ */
+public class InterpreterImpl extends AbstractHandlerBehaviour
+        implements PiPipelineInterpreter {
+
+    private static final int PORT_BITWIDTH = 9;
+
+    private static final ImmutableBiMap<Criterion.Type, PiMatchFieldId> CRITERION_MAP =
+            new ImmutableBiMap.Builder<Criterion.Type, PiMatchFieldId>()
+                    .put(Criterion.Type.IN_PORT, HDR_STANDARD_METADATA_INGRESS_PORT)
+                    .put(Criterion.Type.ETH_DST, HDR_ETHERNET_DST_ADDR)
+                    .put(Criterion.Type.ETH_SRC, HDR_ETHERNET_SRC_ADDR)
+                    .put(Criterion.Type.ETH_TYPE, HDR_ETHERNET_ETHER_TYPE)
+                    .put(Criterion.Type.IPV6_DST, HDR_IPV6_DST_ADDR)
+                    .build();
+
+    @Override
+    public PiAction mapTreatment(TrafficTreatment treatment, PiTableId piTableId)
+            throws PiInterpreterException {
+        if (!piTableId.equals(FABRIC_INGRESS_ACL)) {
+            throw new PiInterpreterException(
+                    "Treatment mapping not supported for table " + piTableId);
+        }
+
+        if (treatment.allInstructions().isEmpty()) {
+            // Zero instructions means drop.
+            return PiAction.builder().withId(FABRIC_INGRESS_DROP).build();
+        } else if (treatment.allInstructions().size() > 1) {
+            // We understand treatments with only 1 instruction.
+            throw new PiInterpreterException("Treatment has too many instructions");
+        }
+
+        Instruction instruction = treatment.allInstructions().get(0);
+        switch (instruction.type()) {
+            case OUTPUT:
+                PortNumber port = ((OutputInstruction) instruction).port();
+                if (port.equals(CONTROLLER)) {
+                    final PiActionId actionId = treatment.clearedDeferred()
+                            ? FABRIC_INGRESS_PUNT_TO_CPU
+                            : FABRIC_INGRESS_CLONE_TO_CPU;
+                    return PiAction.builder().withId(actionId).build();
+                }
+                break;
+            case NOACTION:
+                return PiAction.builder().withId(NO_ACTION).build();
+            default:
+                break;
+        }
+        throw new PiInterpreterException(format(
+                "Treatment mapping not supported for instruction %s", instruction));
+    }
+
+    @Override
+    public Collection<PiPacketOperation> mapOutboundPacket(OutboundPacket packet)
+            throws PiInterpreterException {
+        TrafficTreatment treatment = packet.treatment();
+
+        // Packet-out in srv6.p4 supports only setting the output port,
+        // i.e. OUTPUT instructions.
+        List<OutputInstruction> outInstructions = treatment
+                .allInstructions()
+                .stream()
+                .filter(i -> i.type().equals(OUTPUT))
+                .map(i -> (OutputInstruction) i)
+                .collect(toList());
+
+        if (treatment.allInstructions().size() != outInstructions.size()) {
+            // There are other instructions that are not of type OUTPUT.
+            throw new PiInterpreterException("Treatment not supported: " + treatment);
+        }
+
+        ImmutableList.Builder<PiPacketOperation> builder = ImmutableList.builder();
+        for (OutputInstruction outInst : outInstructions) {
+            if (outInst.port().isLogical() && !outInst.port().equals(FLOOD)) {
+                throw new PiInterpreterException(format(
+                        "Packet-out on logical port '%s' not supported", outInst.port()));
+            } else if (outInst.port().equals(FLOOD)) {
+                // To emulate flooding, we create a packet-out operation for
+                // each switch port.
+                final DeviceService deviceService = handler().get(DeviceService.class);
+                for (Port port : deviceService.getPorts(packet.sendThrough())) {
+                    builder.add(createPacketOp(packet.data(), port.number().toLong()));
+                }
+            } else {
+                // Singleton port.
+                builder.add(createPacketOp(packet.data(), outInst.port().toLong()));
+            }
+        }
+        return builder.build();
+    }
+
+    @Override
+    public InboundPacket mapInboundPacket(PiPacketOperation packetIn, DeviceId deviceId)
+            throws PiInterpreterException {
+
+        Ethernet ethPkt;
+        try {
+            ethPkt = Ethernet.deserializer().deserialize(
+                    packetIn.data().asArray(), 0, packetIn.data().size());
+        } catch (DeserializationException dex) {
+            throw new PiInterpreterException(dex.getMessage());
+        }
+
+        // Returns the ingress port packet metadata.
+        Optional<PiPacketMetadata> packetMetadata = packetIn.metadatas()
+                .stream().filter(m -> m.id().equals(INGRESS_PORT))
+                .findFirst();
+
+        if (packetMetadata.isPresent()) {
+            ImmutableByteSequence portByteSequence = packetMetadata.get().value();
+            short s = portByteSequence.asReadOnlyBuffer().getShort();
+            ConnectPoint receivedFrom = new ConnectPoint(deviceId, PortNumber.portNumber(s));
+            ByteBuffer rawData = ByteBuffer.wrap(packetIn.data().asArray());
+            return new DefaultInboundPacket(receivedFrom, ethPkt, rawData);
+        } else {
+            throw new PiInterpreterException(format(
+                    "Missing metadata '%s' in packet-in received from '%s': %s",
+                    INGRESS_PORT, deviceId, packetIn));
+        }
+    }
+
+    private PiPacketOperation createPacketOp(ByteBuffer data, long portNumber)
+            throws PiInterpreterException {
+        PiPacketMetadata metadata = createPacketMetadata(portNumber);
+        return PiPacketOperation.builder()
+                .withType(PACKET_OUT)
+                .withData(copyFrom(data))
+                .withMetadatas(ImmutableList.of(metadata))
+                .build();
+    }
+
+    private PiPacketMetadata createPacketMetadata(long portNumber) throws PiInterpreterException {
+        try {
+            return PiPacketMetadata.builder()
+                    .withId(EGRESS_PORT)
+                    .withValue(copyFrom(portNumber).fit(PORT_BITWIDTH))
+                    .build();
+        } catch (ImmutableByteSequence.ByteSequenceTrimException e) {
+            throw new PiInterpreterException(format(
+                    "Port number %d too big, %s", portNumber, e.getMessage()));
+        }
+    }
+
+    @Override
+    public Optional<PiMatchFieldId> mapCriterionType(Criterion.Type type) {
+        return Optional.ofNullable(CRITERION_MAP.get(type));
+    }
+
+    @Override
+    public Optional<Criterion.Type> mapPiMatchFieldId(PiMatchFieldId headerFieldId) {
+        return Optional.ofNullable(CRITERION_MAP.inverse().get(headerFieldId));
+    }
+
+    @Override
+    public Optional<PiTableId> mapFlowRuleTableId(int flowRuleTableId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> mapPiTableId(PiTableId piTableId) {
+        return Optional.empty();
+    }
+}
