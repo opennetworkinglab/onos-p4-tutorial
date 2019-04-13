f92b06c1f7e234a5e8196e2588f039becefa4425
Carmelo Cascone <carmelo@ccascone.net>
2019-04-13 01:17:00 -0700

    2x2 Mininet topology with IPv6 hosts

---
+++ mininet/host6.py
@@ -0,0 +1,38 @@
+#  Copyright 2019-present Open Networking Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from mininet.node import Host
+
+
+class IPv6Host(Host):
+
+    def config(self, ipv6, ipv6_gw=None, **params):
+        super(IPv6Host, self).config(**params)
+        self.cmd('ip -4 addr flush dev %s' % self.defaultIntf())
+        self.cmd('ip -6 addr add %s dev %s' % (ipv6, self.defaultIntf()))
+        if ipv6_gw:
+            self.cmd('ip -6 route add default via %s' % ipv6_gw)
+
+    def terminate(self):
+        # self.cmd( 'sysctl -w net.ipv6.conf.all.forwarding=0' )
+        super(IPv6Host, self).terminate()
+
+
+class SRv6Host(IPv6Host):
+
+    def config(self, ipv6, ipv6_gw=None, **params):
+        super(IPv6Host, self).config(**params)
+        # Enable SRv6
+        self.cmd('sysctl -w net.ipv6.conf.all.seg6_enabled=1')
+        self.cmd('sysctl -w net.ipv6.conf.%s.seg6_enabled=1' % self.defaultIntf())
---
+++ mininet/topo.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+#  Copyright 2019-present Open Networking Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import argparse
+
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.topo import Topo
+
+from bmv2 import ONOSStratumSwitch
+from host6 import IPv6Host
+
+
+class TutorialTopo(Topo):
+    """Trellis basic topology"""
+
+    def __init__(self, *args, **kwargs):
+        Topo.__init__(self, *args, **kwargs)
+
+        # Leaves
+        leaf1 = self.addSwitch('leaf1', cls=ONOSStratumSwitch, grpcport=50001,
+                               pipeconf="org.p4.srv6-tutorial")
+        leaf2 = self.addSwitch('leaf2', cls=ONOSStratumSwitch, grpcport=50002,
+                               pipeconf="org.p4.srv6-tutorial")
+
+        # Spines
+        spine1 = self.addSwitch('spine1', cls=ONOSStratumSwitch, grpcport=50003,
+                                pipeconf="org.p4.srv6-tutorial")
+        spine2 = self.addSwitch('spine2', cls=ONOSStratumSwitch, grpcport=50004,
+                                pipeconf="org.p4.srv6-tutorial")
+
+        # Switch Links
+        self.addLink(spine1, leaf1)
+        self.addLink(spine1, leaf2)
+        self.addLink(spine2, leaf1)
+        self.addLink(spine2, leaf2)
+
+        # IPv6 hosts attached to leaf 1
+        h1a = self.addHost('h1a', cls=IPv6Host, mac="00:00:00:00:00:1A",
+                           ipv6='2001:1:1::a/64', ipv6_gw='2001:1:1::ff')
+        h1b = self.addHost('h1b', cls=IPv6Host, mac="00:00:00:00:00:1B",
+                           ipv6='2001:1:1::b/64', ipv6_gw='2001:1:1::ff')
+        h1c = self.addHost('h1c', cls=IPv6Host, mac="00:00:00:00:00:1C",
+                           ipv6='2001:1:1::c/64', ipv6_gw='2001:1:1::ff')
+        h2 = self.addHost('h2', cls=IPv6Host, mac="00:00:00:00:00:20",
+                          ipv6='2001:1:2::a/64', ipv6_gw='2001:1:2::ff')
+        self.addLink(h1a, leaf1)  # port 3
+        self.addLink(h1b, leaf1)  # port 4
+        self.addLink(h1c, leaf1)  # port 5
+        self.addLink(h2, leaf1)  # port 6
+
+        # IPv6 hosts attached to leaf 2
+        h3 = self.addHost('h3', cls=IPv6Host, mac="00:00:00:00:00:30",
+                          ipv6='2001:1:3::1/64', ipv6_gw='2001:1:3::ff')
+        h4 = self.addHost('h4', cls=IPv6Host, mac="00:00:00:00:00:40",
+                          ipv6='2001:1:4::1/64', ipv6_gw='2001:1:4::ff')
+        self.addLink(h3, leaf2)  # port 3
+        self.addLink(h4, leaf2)  # port 4
+
+
+def main(argz):
+    topo = TutorialTopo()
+    controller = RemoteController('c0', ip=argz.onos_ip)
+
+    net = Mininet(topo=topo, controller=None)
+    net.addController(controller)
+
+    net.start()
+    CLI(net)
+    net.stop()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Mininet script for 2x2 fabric with BMv2 and IPv6 hosts')
+    parser.add_argument('--onos-ip', help='ONOS controller IP address',
+                        type=str, action="store", required=True)
+    args = parser.parse_args()
+    setLogLevel('info')
+
+    main(args)
